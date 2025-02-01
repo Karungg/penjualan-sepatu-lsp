48a3a00d26c553d9fb77d7339125cc0fd517a904
Karungg <miftahfadilah71@gmail.com>
2025-02-01 14:46:16 +0700

    Update home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
     <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+            <p class="m-0 text-center text-white">Copyright &copy; Casualin 2024</p>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
